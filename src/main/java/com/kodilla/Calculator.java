6067877a1e1f541d4f9331e0c7036b2c0e635cfc
pfilar <pfilar2@wp.pl>
2019-03-17 15:08:08 +0100

    First Commit

--- src/main/java/com/kodilla/Calculator.java
+++ src/main/java/com/kodilla/Calculator.java
@@ -1,4 +1,14 @@
 package com.kodilla;
 
 public class Calculator {
+
+    public void addition(){
+    }
+
+    public void subtraction() {
+    }
+
+    public static void main(String[] args) {
+
+    }
 }
